3917b368b62a2f776e1414594f17a39df6aeb903
VAMSHI <VAMSHI@LAPTOP-FG9DSLN1>
2024-09-24 13:11:30 +0530

    getters and setters added

--- src/com/edubridge/myappone/model/employee.java
+++ src/com/edubridge/myappone/model/employee.java
@@ -3,5 +3,23 @@ package com.edubridge.myappone.model;
 public class employee {
 		private int employeeId;
 		private String employeeName;
+		public int getEmployeeId() {
+			return employeeId;
+		}
+		public void setEmployeeId(int employeeId) {
+			this.employeeId = employeeId;
+		}
+		public String getEmployeeName() {
+			return employeeName;
+		}
+		public void setEmployeeName(String employeeName) {
+			this.employeeName = employeeName;
+		}
+		public long getEmployeeSalary() {
+			return employeeSalary;
+		}
+		public void setEmployeeSalary(long employeeSalary) {
+			this.employeeSalary = employeeSalary;
+		}
 		private long employeeSalary;
 }
